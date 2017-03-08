02ddb87eb23079f69c0c1b63e9847a1f225e4d0f
Arthur <nullbool@hotmail.com>
2017-03-08 05:07:51 +0200

    new feature (reactjs) create app frame with components for build

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+render(
+    <div>
+        <h2>Welcome to the main page!</h2>
+        <br/>
+        How are you?
+        <br/>
+    </div>,
+    document.getElementById("app-root")
+);
---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+module.exports = {
+    entry: path.join(__dirname, 'src', 'index.js'),
+    output: {
+        path: '/public/',
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loaders: ["react-hot-loader", "babel-loader"]
+            }
+        ]
+    }
+
+
+};
