d8f1f46e69db9e5f7dfb53392f43350745547bd9
Arthur <nullbool@hotmail.com>
2017-03-10 02:48:27 +0200

    update (webpack) addition new configuration for build of project

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,41 @@ module.exports = {
                 test: /\.js$/,
                 exclude: /node_modules/,
                 loaders: ["react-hot-loader", "babel-loader"]
+            },
+            {
+                test: /\.jsx$/,
+                exclude: /node_modules/,
+                loaders: ["react-hot-loader", "babel-loader"]
+            },
+            {
+                test: /\.css$/,
+                loader: "style-loader!css-loader!autoprefixer-loader",
+                exclude: /node_modules/
+            },
+            {
+                test: /\.less $/,
+                loader: "style-loader!css-loader!autoprefixer-loader!less",
+                exclude: /node_modules/
+            },
+            {
+                test: /\.gif$/,
+                loader: "url-loader?limit=10000&mimetype=image/gif"
+            },
+            {
+                test: /\.jpg$/,
+                loader: "url-loader?limit=10000&mimetype=image/jpg"
+            },
+            {
+                test: /\.png$/,
+                loader: "url-loader?limit=10000&mimetype=image/png"
+            },
+            {
+                test: /\.svg/,
+                loader: "url-loader?limit=26000&mimetype=image/svg+xml"
+            },
+            {
+                test: /\.json$/,
+                loader: "json-loader"
             }
         ]
     }
