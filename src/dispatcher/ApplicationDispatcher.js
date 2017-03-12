a1b5c456d17a27df54312804ed385f129da34a82
Arthur <nullbool@hotmail.com>
2017-03-12 15:07:21 +0200

    create dispatcher listeners and event executer

--- src/dispatcher/ApplicationDispatcher.js
+++ src/dispatcher/ApplicationDispatcher.js
@@ -1,3 +1,22 @@
-import { Dispatcher } from 'flux';
 
-export default new Dispatcher();
+    const listeners = {};
+
+    export function listen(action, fn) {
+
+        listeners[action] = listeners[action] || [];
+
+        listeners[action].push(fn);
+
+    }
+
+    export function emit(action, ...data) {
+
+        //console.log('emit', action, ...data);
+
+        const fns = listeners[action];
+
+        if(fns) {
+            fns.forEach( (fn) => fn(...data));
+        }
+
+    }
