96d2da3d6df1981a78bc095c9c59c5a7b02802b0
Arthur <nullbool@hotmail.com>
2017-03-13 01:43:45 +0200

    remove pager html block

--- src/components/NewsList.jsx
+++ src/components/NewsList.jsx
@@ -54,28 +54,6 @@ class NewsList extends React.Component {
                         <tbody>
                         { list }
                         </tbody>
-                        <tfoot>
-                        <tr>
-                            <td>
-                                <div className="row">
-                                    <div className="col-md-24 text-center">
-                                        <div className="pagination">
-                                            <ul className="pagination">
-                                                <li><a href="#">&laquo;</a></li>
-                                                <li><a href="#">1</a></li>
-                                                <li><a href="#">2</a></li>
-                                                <li><a href="#">3</a></li>
-                                                <li><a href="#">4</a></li>
-                                                <li><a href="#">5</a></li>
-                                                <li><a href="#">&raquo;</a></li>
-                                            </ul>
-                                        </div>
-                                    </div>
-                                </div>
-                            </td>
-                        </tr>
-                        </tfoot>
-
                     </table>
                 </section>
             </div>
